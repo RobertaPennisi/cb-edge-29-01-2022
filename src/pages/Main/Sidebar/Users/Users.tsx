1a7c23f9e9c69cf72a8c253cc9fd7b23f706eb5f
Roberta Pennisi <roberta.pennisi97@gmail.com>
2022-01-29 11:15:43 +0100

    struttura cartelle

---
+++ src/pages/Main/Sidebar/Users/Users.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display:flex;
+    height: 100px;
+    widht: 100px;
+    background-color: red;
+`;
+
+const Users = () => {
+    return(
+        <Container>
+            
+        </Container>
+    )
+};
